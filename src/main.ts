d2313b58454c957b7a8462d40ccd7da7d686c20a
xushengfeng <xushengfeng_zg@163.com>
2025-09-07 22:28:38 +0800

    类型 泛型

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,35 @@
 export { env, newNativeFunction };
 
-type XType = { main: "string" | "num" };
+type XType = (
+	| { type: "string" }
+	| { type: "num" }
+	| { type: "array"; subType: { item: XType } }
+	| { type: "object"; subType: { value: XType } }
+	| { type: "bool" }
+	| { type: "stop" }
+	| { type: "nil" }
+	| { type: "any" }
+) & { name?: string };
 
-type XTypeToActual<T extends XType> = T extends { main: "string" }
+type XTypeToActual<T extends XType> = T["type"] extends "string"
 	? string
-	: T extends { main: "num" }
+	: T["type"] extends "num"
 		? number
-		: never;
+		: T["type"] extends "array"
+			? // @ts-expect-error
+				XTypeToActual<T["subType"]["item"]>[]
+			: T["type"] extends "object"
+				? // @ts-expect-error
+					Record<string, XTypeToActual<T["subType"]["value"]>>
+				: T["type"] extends "bool"
+					? boolean
+					: T["type"] extends "stop"
+						? never
+						: T["type"] extends "nil"
+							? null
+							: T["type"] extends "any"
+								? unknown
+								: never;
 
 type InputToArgs<I extends { name: string; type: XType }[]> = {
 	[K in I[number]["name"]]: XTypeToActual<
@@ -41,13 +64,18 @@ function newNativeFunction<
 
 newNativeFunction({
 	input: [
-		{ name: "str", type: { main: "string" } },
-		{ name: "arr", type: { main: "num" } },
+		{ name: "str", type: { type: "string" } },
+		{ name: "arr", type: { type: "num" } },
 	],
-	output: [{ name: "x", type: { main: "string" } }],
+	output: [
+		{
+			name: "x",
+			type: { type: "array", subType: { item: { type: "string" } } },
+		},
+	],
 	fun: (args) => {
 		args.str;
-		return { x: "" };
+		return { x: [] };
 	},
 });
 
@@ -65,20 +93,56 @@ type XFunction = {
 
 const nativeFunctions: Record<string, NativeFunction> = {
 	"str.split": newNativeFunction({
-		input: [{ name: "str", type: { main: "string" } }],
-		output: [{ name: "arr", type: { main: "string" } }],
+		input: [{ name: "str", type: { type: "string" } }],
+		output: [
+			{
+				name: "arr",
+				type: { type: "array", subType: { item: { type: "string" } } },
+			},
+		],
 		fun: (args) => ({
 			arr: args.str.split(" "),
 		}),
 	}),
 	"str.join": newNativeFunction({
 		input: [
-			{ name: "arr", type: { main: "string" } },
-			{ name: "sep", type: { main: "string" } },
+			{
+				name: "arr",
+				type: { type: "array", subType: { item: { type: "string" } } },
+			},
+			{ name: "sep", type: { type: "string" } },
 		],
-		output: [{ name: "str", type: { main: "string" } }],
+		output: [{ name: "str", type: { type: "string" } }],
 		fun: (args) => ({
 			str: args.arr.join(args.sep),
+		}),
+	}),
+	"array.map": newNativeFunction({
+		input: [
+			{
+				name: "arr",
+				type: { type: "array", subType: { item: { type: "any", name: "I" } } },
+			},
+		],
+		output: [
+			{
+				name: "arr",
+				type: { type: "array", subType: { item: { type: "any", name: "O" } } },
+			},
+		],
+		cb: {
+			cb: {
+				input: [
+					{
+						name: "item",
+						type: { type: "any", name: "I" },
+					},
+				],
+				output: [{ name: "callback", type: { type: "any", name: "O" } }],
+			},
+		},
+		fun: (args, cb) => ({
+			arr: args.arr.map(cb.cb),
 		}),
 	}),
 };
